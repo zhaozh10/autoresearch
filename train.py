878a206820aa691cd89d65f866ade6d88855dc5d
zhaozh10 <1722586026@qq.com>
2026-03-26 07:11:29 +0000

    exp: FINAL_LR_FRAC 0.0->0.1 (floor LR at 10% of peak)

--- train.py
+++ train.py
@@ -444,7 +444,7 @@ WEIGHT_DECAY = 0.2      # cautious weight decay for Muon
 ADAM_BETAS = (0.8, 0.95) # Adam beta1, beta2
 WARMUP_RATIO = 0.0      # fraction of time budget for LR warmup
 WARMDOWN_RATIO = 0.5    # fraction of time budget for LR warmdown
-FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
+FINAL_LR_FRAC = 0.1     # final LR as fraction of initial
 
 # Model size
 DEPTH = 8               # number of transformer layers
