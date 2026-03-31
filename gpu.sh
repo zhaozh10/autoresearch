fcf2680386396c1c60d608f9edeb7c51c71224e5
zhaozh10 <1722586026@qq.com>
2026-03-31 15:48:46 +0200

    update

--- gpu.sh
+++ gpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=radcluster
 #SBATCH --job-name=auto
-#SBATCH --time=24:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --nodelist=hawking
 #SBATCH --cpus-per-task=6
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --mem=120G
 
 nvidia-smi
